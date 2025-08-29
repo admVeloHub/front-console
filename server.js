dacdec3d48c50f37067b25c786bdecdd90d08300
admVeloHub <lucas.gravina@velotax.com.br>
2025-08-29 19:15:40 -0300

    fix: Removidas referências ao localhost e criada página de status dedicada para o backend

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.use(express.static('public'));
 
 // Rota raiz - servir página de status do backend
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/backend-status.html');
 });
 
 // API - Inserir dados
